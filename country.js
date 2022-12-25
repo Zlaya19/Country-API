13438080b18c3c538753a1e41d6cb0b4876f2335
Zlaya19 <zlatangavranovic1906@gmail.com>
2022-12-25 11:36:45 +0100

    adding images

--- country.js
+++ country.js
@@ -16,10 +16,10 @@ function getTheText(e) {
             apiValue = JSON.parse(this.responseText);
             output.classList.add('classWord');
             output.innerHTML = `<span1>${apiValue[0].name}</span1>`;
-            output.innerHTML += `<img src="${apiValue[0].flag} "width="270px">` + "<br>";
+            output.innerHTML += `<img src="${apiValue[0].flag} "width="300px">` + "<br>";
             output.innerHTML += `<span>capital</span>` + apiValue[0].capital +"<br>";
             output.innerHTML += `<span>population</span>` + apiValue[0].population +"<br>";
-            output.innerHTML += `<span>borders</span>` + apiValue[0].borders +"<br>";
+            /* output.innerHTML += `<span>borders</span>` + apiValue[0].borders +"<br>"; */
             output.innerHTML += `<span>currencies</span>` + apiValue[0].currencies[0].name +"<br>";
             output.innerHTML += `<span>native name</span>` + apiValue[0].languages[0].nativeName;
         }
